3ba744976f74741a55f2c99f7e15c171fb1b12b2
daalekz <dontdissthesonic123@gmail.com>
2019-01-30 12:05:07 +1100

    finalise

--- src/com/nish/MainRunner.java
+++ src/com/nish/MainRunner.java
@@ -25,7 +25,7 @@ public class MainRunner
         //wait for the client to be logged in before polling what guilds the bot is in
         try
         {
-            Thread.sleep(2000); //if the list is being output as blank, increase this number. 
+            Thread.sleep(2000); //if the list is being output as blank, increase this number.
         }
         catch (java.lang.InterruptedException e)
         {
