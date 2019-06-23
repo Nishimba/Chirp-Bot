73c353066c5c64626a4e570161f2c3ea3aaae084
daalekz <dontdissthesonic123@gmail.com>
2019-06-23 22:09:33 +1000

    removed unneeded comment

--- src/com/nish/TimeUtils.java
+++ src/com/nish/TimeUtils.java
@@ -11,7 +11,6 @@ import java.time.*;
 public class TimeUtils {
     private static final String TIME_FORMAT = "hh:mm a";
 
-    //TODO add any member variables required for pretty output
 
     public String convertTime(ZoneId source, ZoneId destination, ZonedDateTime time){
         ZonedDateTime destTime = time.withZoneSameInstant(destination) ; // Same moment adjusted into another time zone.
